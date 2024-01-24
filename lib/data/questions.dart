8d44c9e53b04e3dd478365acc0c661c220209206
Eric Nguyen <enguyen807@gmail.com>
2024-01-24 18:05:08 +0200

    Add questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,47 @@
+import 'package:quiz_app/models/app_quiz_question.dart';
+
+const questions = [
+  AppQuizQuestion(
+    'Kas te ____ eesti keelt?', 
+    [
+      'räägite',
+      'räägin',
+      'räägivad',
+      'räägib'
+    ]
+  ),
+  AppQuizQuestion(
+    'Which month comes right after September', [
+      'Oktoober',
+      'Jaanuar',
+      'Mai',
+      'Juuli'
+    ]
+  ),
+  AppQuizQuestion(
+    'What is often made of paper?', [
+      'Raamat',
+      'Päikeseprillid',
+      'Krediitkart',
+      'Kruus'
+    ]
+  ),
+  AppQuizQuestion(
+    'Mul on _____ hea meel teiega tutvuda.', 
+    [
+      'väga',
+      'aeglasemalt',
+      'täpselt',
+      'olete'
+    ]
+  ),
+  AppQuizQuestion(
+    'Mis on pealinn Eesti?', 
+    [
+      'Tallinn',
+      'Tartu',
+      'Pärnu',
+      'Narva'
+    ]
+  )
+];
---
+++ lib/models/app_quiz_question.dart
@@ -0,0 +1,6 @@
+class AppQuizQuestion {
+  const AppQuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+}
